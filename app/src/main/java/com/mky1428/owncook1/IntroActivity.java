7492eac95200f1d4b3b30029574bcf3da559b4c4
mky1428 <mky1428@gmail.com>
2021-09-08 15:36:19 +0900

    인트로액티비티에 애니메이션 효과 삽입

--- app/src/main/java/com/mky1428/owncook1/IntroActivity.java
+++ app/src/main/java/com/mky1428/owncook1/IntroActivity.java
@@ -3,12 +3,27 @@ package com.mky1428.owncook1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class IntroActivity extends AppCompatActivity {
+
+    ImageView iv;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        iv = findViewById(R.id.iv);
+        tv = findViewById(R.id.tv);
+
+        Animation ani = AnimationUtils.loadAnimation(this, R.anim.appear_logo);
+        iv.startAnimation(ani);
+        tv.startAnimation(ani);
+
     }
 }
