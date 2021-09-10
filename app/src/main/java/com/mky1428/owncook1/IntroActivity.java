90603fb11c38da7e00d8a02c5933b5520e60bf2f
mky1428 <mky1428@gmail.com>
2021-09-10 13:39:05 +0900

    인트로액티비 애니메이션 종료 후 다음 액티비티로 전환

--- app/src/main/java/com/mky1428/owncook1/IntroActivity.java
+++ app/src/main/java/com/mky1428/owncook1/IntroActivity.java
@@ -2,6 +2,7 @@ package com.mky1428.owncook1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -24,6 +25,27 @@ public class IntroActivity extends AppCompatActivity {
         Animation ani = AnimationUtils.loadAnimation(this, R.anim.appear_logo);
         iv.startAnimation(ani);
         tv.startAnimation(ani);
+
+        ani.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
     }
 }
