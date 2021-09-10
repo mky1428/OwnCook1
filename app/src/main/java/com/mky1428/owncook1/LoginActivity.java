46c2dee0ecc3d4a08f0f2d0fb072085d7bee1b3e
mky1428 <mky1428@gmail.com>
2021-09-10 16:17:49 +0900

    activity_login.xml 구글 로그인 버튼 작업

--- app/src/main/java/com/mky1428/owncook1/LoginActivity.java
+++ app/src/main/java/com/mky1428/owncook1/LoginActivity.java
@@ -3,6 +3,7 @@ package com.mky1428.owncook1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void clickKakaoLogin(View view) {
+    }
+
+    public void clickGoogleLogin(View view) {
     }
 }
