6a8a518a7e1f0fb738b535905bd39dbe667c2aba
mky1428 <mky1428@gmail.com>
2021-09-11 22:49:11 +0900

    activity_register.xml 작업 중간 커밋1

--- app/src/main/java/com/mky1428/owncook1/LoginActivity.java
+++ app/src/main/java/com/mky1428/owncook1/LoginActivity.java
@@ -2,6 +2,7 @@ package com.mky1428.owncook1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,9 +14,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    public void clickRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
     public void clickKakaoLogin(View view) {
     }
 
     public void clickGoogleLogin(View view) {
     }
+
+
 }
