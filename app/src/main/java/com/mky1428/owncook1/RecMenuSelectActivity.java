169528db4b19c60268eed2181a90bfef51748e94
mky1428 <mky1428@gmail.com>
2021-09-17 15:06:37 +0900

    RecMenuSelectActivity 작업 시작

---
+++ app/src/main/java/com/mky1428/owncook1/RecMenuSelectActivity.java
@@ -0,0 +1,18 @@
+package com.mky1428.owncook1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RecMenuSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rec_menu_select);
+    }
+
+    public void clickParse(View view) {
+    }
+}
